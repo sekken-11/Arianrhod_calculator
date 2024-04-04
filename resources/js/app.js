3902a00d761d4b76dbb19fc12089e809824187f8
sekken11 <soratorobo0611@gmail.com>
2024-04-05 02:24:05 +0900

    vuetify導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+
 import App from '../views/vue/App.vue'
-createApp(App).mount("#app")
+
+const app = createApp(App);
+const vuetify = createVuetify();
+
+app.use(vuetify) 
+app.mount('#app');
 
 import Alpine from 'alpinejs';
 
